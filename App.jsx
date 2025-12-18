2852f6a6c2ee629abf4faefbfa15f766678068e6
Rajeshwari <rajarajeshwari734@gmail.com>
2025-12-18 12:53:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Course from "./Fullstackproject/Sample/Course";
+
+const App = () => {
+  return (
+    <div>
+      <Course />
+    </div>
+  );
+};
+
+export default App;
