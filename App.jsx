514c09c458f9449aaa5ee2fd68b2be2e20ad6d70
Rajeshwari <rajarajeshwari734@gmail.com>
2025-12-18 12:56:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Feedback from "./Fullstackproject/Sample/Feedback";
+const App = () => {
+  return (
+    <div>
+      <Feedback />
+    </div>
+  );
+};
+export default App;
